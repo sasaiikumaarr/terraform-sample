c59d0377a172230abf42c8df350a45544d28899d
Sasikumar <sasiikumaarr@gmail.com>
2020-07-03 22:01:56 +0530

    added security group and key pair

--- modules/ec2/launch-ec2.tf
+++ modules/ec2/launch-ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "sample-terraform-instance" {
     ami = "ami-02b5d851009884e20"
     instance_type = "t2.micro"
     subnet_id = "subnet-04678db983bfbcf4c"
+    security_groups = ["sg-ea4a19c1"]
+    key_name = "aws-test-key"
     tags = {
         Name = "terraform-instance"
     }
