c0c38b253fb7a8b705e8597cc4e04adcda43532d
Sasikumar <sasiikumaarr@gmail.com>
2020-07-03 22:13:49 +0530

    added profile

--- modules/ec2/launch-ec2.tf
+++ modules/ec2/launch-ec2.tf
@@ -2,8 +2,9 @@ resource "aws_instance" "sample-terraform-instance" {
     ami = "ami-02b5d851009884e20"
     instance_type = "t2.micro"
     subnet_id = "subnet-04678db983bfbcf4c"
-    security_groups = ["sg-ea4a19c1"]
+    security_group = ["sg-ea4a19c1"]
     key_name = "aws-test-key"
+    iam_instance_profile = "ec2-role"
     tags = {
         Name = "terraform-instance"
     }
