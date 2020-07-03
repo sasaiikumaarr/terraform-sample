b0aa9807805cea92ea57bf171da32a37cafe4519
Sasikumar <sasiikumaarr@gmail.com>
2020-07-03 21:22:03 +0530

    added subnet id for ec2 instance

--- modules/ec2/launch-ec2.tf
+++ modules/ec2/launch-ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample-terraform-instance" {
     ami = "ami-02b5d851009884e20"
     instance_type = "t2.micro"
+    subnet_id = "subnet-04678db983bfbcf4c"
     tags = {
         Name = "terraform-instance"
     }
